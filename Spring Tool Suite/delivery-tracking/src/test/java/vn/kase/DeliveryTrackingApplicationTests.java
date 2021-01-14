3479afd93dcf425b455f30c1bf86446cf3526a90
Hao Nhut La <trannhuthao997@gmail.com>
2021-01-14 21:24:19 +0700

    Initial commit.

---
+++ Spring Tool Suite/delivery-tracking/src/test/java/vn/kase/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.kase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
