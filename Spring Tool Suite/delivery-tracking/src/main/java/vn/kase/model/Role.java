bf05be80b6d4363032ac6c6e4c217b36a20997ef
Hieu Minh Le <hieucoder@outlook.com>
2021-01-16 18:39:32 +0700

    Added Role domain model.

---
+++ Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/model/Role.java
@@ -0,0 +1,26 @@
+package vn.kase.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "description")
+    private String description;
+}
