27fab99915f93130f2b9c9ce967d9c09cd7a3c14
Hieu Minh Le <hieucoder@outlook.com>
2021-01-22 20:01:12 +0700

    Implemented Service layer for OrderDetail domain model

--- Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/service/v1/impl/OrderDetailServiceImpl.java
+++ Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/service/v1/impl/OrderDetailServiceImpl.java
@@ -1,8 +1,61 @@
 package vn.kase.service.v1.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.kase.domain.v1.OrderDetail;
+import vn.kase.domain.v1.Package;
+import vn.kase.dto.v1.mapper.OrderDetailMapper;
+import vn.kase.dto.v1.mapper.PackageMapper;
+import vn.kase.dto.v1.model.OrderDetailDto;
+import vn.kase.dto.v1.model.PackageDto;
+import vn.kase.repository.v1.OrderDetailRepository;
 import vn.kase.service.v1.OrderDetailService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderDetailServiceImpl implements OrderDetailService {
+    private final OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    public OrderDetailServiceImpl(OrderDetailRepository orderDetailRepository) {
+        this.orderDetailRepository = orderDetailRepository;
+    }
+
+    @Override
+    public void add(OrderDetailDto orderDetailDto) {
+        this.orderDetailRepository.save(OrderDetailMapper.toEntity(orderDetailDto));
+    }
+
+    @Override
+    public List<OrderDetailDto> findAll() {
+        List<OrderDetail> orderDetailList = this.orderDetailRepository.findAll();
+        List<OrderDetailDto> orderDetailDtos = new ArrayList<OrderDetailDto>();
+        for (OrderDetail orderDetail : orderDetailList) {
+            orderDetailDtos.add(OrderDetailMapper.toDto(orderDetail));
+        }
+
+        return orderDetailDtos;
+    }
+
+    @Override
+    public OrderDetailDto findById(Long id) {
+        return OrderDetailMapper.toDto(this.orderDetailRepository.findById(id).get());
+    }
+
+    @Override
+    public void update(OrderDetailDto orderDetailDto) {
+        OrderDetail orderDetail = this.orderDetailRepository.findById(orderDetailDto.getId()).get();
+        orderDetail.setUser(orderDetailDto.getUser());
+        orderDetail.setShippingPackage(orderDetailDto.getShippingPackage());
+        orderDetail.setStatus(orderDetailDto.getStatus());
+        orderDetail.setShipper(orderDetailDto.getShipper());
+        this.orderDetailRepository.save(orderDetail);
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.orderDetailRepository.deleteById(id);
+    }
 }
