5ca0300297cc0d3dfc02354d9dfffef028389354
Hieu Minh Le <hieucoder@outlook.com>
2021-01-28 21:20:02 +0700

    Implemented view for User domain model.

--- Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/repository/v1/DeliveryAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.kase.repository.v1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vn.kase.domain.v1.DeliveryAddress;
-
-@Repository
-public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
-}
