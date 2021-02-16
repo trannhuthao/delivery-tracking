f45a89a8f1a7d3643cecbbce6d188d6b8d78d593
Hieu Minh Le <hieucoder@outlook.com>
2021-02-16 09:39:07 +0700

    Implemented Sending Emails Functionality.

--- Java Back-end/Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/util/email/EmailConstants.java
+++ Java Back-end/Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/util/email/EmailConstants.java
@@ -1,7 +1,7 @@
 package vn.kase.util.email;
 
 public class EmailConstants {
-    public static final String GMAIL = "hieucomputerprogrammer@gmail.com";
-    public static final String GMAIL_PASSWORD = "baxacuaanh0751961";
-    public static final String[] RECEIVERS_EMAIL = { "hieu.minhle@outlook.com"/*, "hieucoder@outlook.com"*/ };
+    public static final String GMAIL = "";
+    public static final String GMAIL_PASSWORD = "";
+    public static final String[] RECEIVERS_EMAIL = {  };
 }
