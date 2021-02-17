1a8eca6777fb05a9088bb7932c41cfd11619c059
Hieu Minh Le <hieucoder@outlook.com>
2021-02-17 12:20:48 +0700

    Implemented 404 and 403 HTML error pages.

--- Java Back-end/Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/config/v1/SecurityConfig.java
+++ Java Back-end/Spring Tool Suite/delivery-tracking/src/main/java/vn/kase/config/v1/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/resources/**", "/static/**", "/css/**", "/js/**").permitAll()
                 .antMatchers("/**").hasRole("PROGRAMMER")
                 .antMatchers("/shipping-packages/*").hasRole("USER")
                 .and().formLogin()
